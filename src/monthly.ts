eebfe66335a435d4012bd68e3c0845e4d1f209f9
Garrick <ndf_724@hotmail.com>
2022-10-06 20:48:08 +0800

    Added test case

--- src/monthly.ts
+++ src/monthly.ts
@@ -2,9 +2,9 @@ import { isPercentage } from './util';
 
 export function getMonthlyContribution(
   monthlySalary: number,
-  employeeContributionRate: number = 11,
+  workerRate: number = 11,
   option?: {
-    overwriteEmployerContribution?: number;
+    overwriteEmployerRate?: number;
   }
 ) {
   if (monthlySalary <= 0) {
@@ -15,29 +15,31 @@ export function getMonthlyContribution(
    * Rate from KWSP Portals
    * @URL https://www.kwsp.gov.my/member/overview#:~:text=When%20you%20contribute%2011%25%20of,government)%20to%20your%20EPF%20savings.
    */
-  let employerContributionRate = monthlySalary <= 5000 ? 13 : 12;
+  let employerRate = monthlySalary <= 5000 ? 13 : 12;
   // Overwrite employer contribution
-  if (option?.overwriteEmployerContribution) {
-    if (!isPercentage(option.overwriteEmployerContribution)) {
-      throw new Error('Empoyer contribution must be a valid percentage');
+  if (option?.overwriteEmployerRate) {
+    if (!isPercentage(option.overwriteEmployerRate)) {
+      throw new Error('overwriteEmployerRate must be a valid percentage');
     }
 
-    employeeContributionRate = option.overwriteEmployerContribution;
+    employerRate = option.overwriteEmployerRate;
   }
 
-  if (!isPercentage(employeeContributionRate)) {
-    throw new Error('employeeContributionRate rate must be a valid percentage');
+  if (!isPercentage(workerRate)) {
+    throw new Error('workerRate rate must be a valid percentage');
   }
 
-  const totalContributedByEmployee =
-    (monthlySalary * employeeContributionRate) / 100;
-  const totalContributedByEmployer =
-    (monthlySalary * employerContributionRate) / 100;
+  const totalContributedByEmployee = (monthlySalary * workerRate) / 100;
+  const totalContributedByEmployer = (monthlySalary * employerRate) / 100;
   const total = totalContributedByEmployee + totalContributedByEmployer;
 
   return {
     total: total,
-    employee: totalContributedByEmployee,
+    worker: totalContributedByEmployee,
     employer: totalContributedByEmployer,
+    rate: {
+      worker: workerRate,
+      employer: employerRate,
+    },
   };
 }
